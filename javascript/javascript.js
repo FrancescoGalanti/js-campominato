32e04f384c00d75beff139a0e5023abf67bf6f9b
FrancescoGalanti <galanti_francesco>
2020-10-23 14:24:52 +0200

    scelta gioctore fatta

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -18,6 +18,20 @@
 
  console.log(bombe)
 
+ var scelta = [];
+ var tries = 84;
+
+
+ while(scelta.length < tries){
+      var num = parseInt(prompt("Scegli un numero compreso tra 1 e 100 e non duplicarli"));
+
+      if( ! scelta.includes(num)){
+        scelta.push(num);
+      }
+ }
+
+ console.log(scelta)
+
 
 
  function random(min,max) {
