ac1e131656568951154cea8adf686098b72debdd
FrancescoGalanti <galanti_francesco>
2020-10-23 16:26:47 +0200

    switch

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -65,19 +65,115 @@
 
  switch(difficolta) {
    case "0":
-       var num = parseInt(prompt("Scegli un numero compreso tra 1 e 100 e non duplicarli"));
-       var number = random(1, 100);
-       var tries = 84;
+
+   var bombe = [];
+   var numerobombe = 16;
+
+   while(bombe.length < numerobombe){
+        var number = random(1, 100);
+
+        if(! bombe.includes(number)){
+          bombe.push(number);
+        }
+   }
+
+   console.log(bombe)
+
+   var scelta = [];
+   var tries = 84;
+
+
+   while(scelta.length < tries){
+        var num = parseInt(prompt("Scegli un numero compreso tra 1 e 100 e non duplicarli"));
+
+        if( ! scelta.includes(num)){
+          scelta.push(num);
+        }
+   }
+
+   console.log(scelta)
+
+   if(bombe.includes(num) === true){
+     alert("hai perso");
+
+
+   } else{
+      alert("hai vinto");
+   }
        break;
    case "1":
+
+   var bombe = [];
+   var numerobombe = 16;
+
+   while(bombe.length < numerobombe){
+        var number = random(1, 80);
+
+        if(! bombe.includes(number)){
+          bombe.push(number);
+        }
+   }
+
+   console.log(bombe)
+
+   var scelta = [];
+   var tries = 64;
+
+
+   while(scelta.length < tries){
         var num = parseInt(prompt("Scegli un numero compreso tra 1 e 80 e non duplicarli"));
-        var number = random(1,80)
-        var tries = 64;
+
+        if( ! scelta.includes(num)){
+          scelta.push(num);
+        }
+   }
+
+   console.log(scelta)
+
+   if(bombe.includes(num) === true){
+     alert("hai perso");
+
+
+   } else{
+      alert("hai vinto");
+   }
        break;
    case "2":
-        var num = parseInt(prompt("Scegli un numero compreso tra 1 e 50 e non duplicarli"));
-        var number = random(1,50)
-        var tries = 36;
+
+   var bombe = [];
+   var numerobombe = 16;
+
+   while(bombe.length < numerobombe){
+        var number = random(1, 50);
+
+        if(! bombe.includes(number)){
+          bombe.push(number);
+        }
+   }
+
+   console.log(bombe)
+
+   var scelta = [];
+   var tries = 34;
+
+
+   while(scelta.length < tries){
+        var num = parseInt(prompt("Scegli un numero compreso tra 1 e 80 e non duplicarli"));
+
+        if( ! scelta.includes(num)){
+          scelta.push(num);
+        }
+   }
+
+   console.log(scelta)
+
+   if(bombe.includes(num) === true){
+     alert("hai perso");
+
+
+   } else{
+      alert("hai vinto");
+   }
         break;
 
  }
